799c37486e66041af759d76d9a88fbc04db7fe4c
McNugget <alexd84a@gmail.com>
2024-05-27 19:52:04 +0200

    mukk

--- ZigSelector/Utility/Helper.cs
+++ ZigSelector/Utility/Helper.cs
@@ -9,7 +9,7 @@ public class Helper
     {
         const string CONFIG_FILE = "config.json";
 
-        FileInfo configFile = new FileInfo(Path.Combine(AppContext.BaseDirectory, CONFIG_FILE));
+        FileInfo configFile = new(Path.Combine(AppContext.BaseDirectory, CONFIG_FILE));
 
         if (!configFile.Exists)
             throw new FileNotFoundException($"Missing configuration file: {configFile.FullName}.");
@@ -19,9 +19,9 @@ public class Helper
             .AddJsonFile(CONFIG_FILE, optional: false, reloadOnChange: true)
             .Build();
 
-        IConfigurationSection bajs = configuration.GetSection("Configuration");
+        IConfigurationSection configSection = configuration.GetSection("Configuration");
         Configuration config = new();
-        bajs.Bind(config);
+        configSection.Bind(config);
         return config;
     }
 }
--- ZigSelector/VersionSelector.cs
+++ ZigSelector/VersionSelector.cs
@@ -10,19 +10,64 @@ public class VersionSelector
 
     public State State { get => _state; }
 
+    public int Index { get => _index; set => _index = value; }
+
     private readonly State _state;
     private readonly DirectoryInfo[] _paths;
     private readonly Configuration _config;
-    private readonly short _zigIndex;
+
+    private DirectoryInfo? _currentBinary;
+    private int _index;
+    private int _zigIndex;
 
     public VersionSelector()
     {
         _paths = GeneratePaths();
         _state = _paths.Length != 0 ? State.Active : State.Terminate;
         _config = Helper.GetConfiguration();
+        _index = -1;
         _zigIndex = -1;
 
+        ListBinaries();
         //UpdatePath(new DirectoryInfo("korv"));
+    }
+
+    private void ListBinaries()
+    {
+        if (!Directory.Exists(_config.BinaryFolder))
+            throw new DirectoryNotFoundException("");
+
+        bool pathFound = false;
+
+        int index = 0;
+
+        foreach (DirectoryInfo zigBin in new DirectoryInfo(_config.BinaryFolder).GetDirectories("*"))
+        {
+            if (!pathFound)
+            {
+                LocateZigPath(zigBin, out pathFound);
+                _index = index;
+            }
+
+            Console.WriteLine($" {(_currentBinary is not null && zigBin.FullName == _currentBinary.FullName ? ">>" : "  ")} {zigBin.FullName}");
+            index++;
+        }
+    }
+
+    private void LocateZigPath(DirectoryInfo zigBin, out bool pathFound)
+    {
+        pathFound = false;
+
+        for (int i = 0; i < _paths.Length; i++)
+        {
+            if (_paths[i].FullName != zigBin.FullName)
+                continue;
+
+            _zigIndex = i;
+            _currentBinary = _paths[i];
+            pathFound = true;
+            break;
+        }
     }
 
     private DirectoryInfo[] GeneratePaths()
@@ -53,7 +98,8 @@ public class VersionSelector
             newPath.Append($"{_paths[i].FullName.Replace("\\", "\\\\")};");
         }
 
-        ;
         Environment.SetEnvironmentVariable("PATH", newPath.ToString()[..^1]); // ta bort sista ;
+
+
     }
 }

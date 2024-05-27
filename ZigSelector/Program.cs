67c19995bd17f45951aacfe811346b2f5849bf00
McNugget <alexd84a@gmail.com>
2024-05-27 19:14:22 +0200

    le korv

--- ZigSelector/Program.cs
+++ ZigSelector/Program.cs
@@ -4,7 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
         try
         {
             VersionSelector zigSelector = new();
--- ZigSelector/VersionSelector.cs
+++ ZigSelector/VersionSelector.cs
@@ -1,30 +1,59 @@
-﻿using ZigSelector.ConfigModel;
+﻿using System.Text;
+using ZigSelector.ConfigModel;
 using ZigSelector.Utility;
 
 namespace ZigSelector;
 
 public class VersionSelector
 {
+    const string PATH = "PATH";
+
     public State State { get => _state; }
 
     private readonly State _state;
     private readonly DirectoryInfo[] _paths;
     private readonly Configuration _config;
+    private readonly short _zigIndex;
 
     public VersionSelector()
     {
         _paths = GeneratePaths();
         _state = _paths.Length != 0 ? State.Active : State.Terminate;
         _config = Helper.GetConfiguration();
+        _zigIndex = -1;
+
+        //UpdatePath(new DirectoryInfo("korv"));
     }
 
     private DirectoryInfo[] GeneratePaths()
     {
         const char SEPARATOR = ';';
-        string? paths = Environment.GetEnvironmentVariable("PATH");
+        string? paths = Environment.GetEnvironmentVariable(PATH);
         if (paths is null)
             return [];
 
         return paths.Split(SEPARATOR, StringSplitOptions.RemoveEmptyEntries).Select(x => new DirectoryInfo(x)).ToArray();
+    }
+
+    public void UpdatePath(DirectoryInfo newZigPath)
+    {
+        if (!newZigPath.Exists)
+            throw new DirectoryNotFoundException("Selected path does not exist");
+
+        StringBuilder newPath = new();
+
+        for (int i = 0; i < _paths.Length; i++)
+        {
+            if (i == _zigIndex)
+            {
+                newPath.Append($"{newZigPath.FullName};");
+                continue;
+            }
+
+            newPath.Append($"{_paths[i].FullName.Replace("\\", "\\\\")};");
+        }
+
+        ;
+        Environment.SetEnvironmentVariable("PATH", newPath.ToString()[..^1]); // ta bort sista ;
     }
 }
